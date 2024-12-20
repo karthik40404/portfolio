5ba7aade3c713e1ba9052a590a10a7213d2f8d88
karthik40404 <karpython123@gmail.com>
2024-12-20 11:31:19 +0530

    bbb

---
+++ portpro/static/js/scripts.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("sideNav").style.width = "250px";
+    document.querySelector(".main-content").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("sideNav").style.width = "0";
+    document.querySelector(".main-content").style.marginLeft = "0";
+}
