f9b048b358c9d05effca10fa9eb16d98cfd0abca
karthik40404 <karpython123@gmail.com>
2024-12-21 11:38:03 +0530

    iiii

--- portpro/static/js/scripts.js
+++ portpro/static/js/scripts.js
@@ -7,3 +7,6 @@ function closeNav() {
     document.getElementById("sideNav").style.width = "0";
     document.querySelector(".main-content").style.marginLeft = "0";
 }
+function toggleDetails(card) {
+    card.classList.toggle("open");
+}
